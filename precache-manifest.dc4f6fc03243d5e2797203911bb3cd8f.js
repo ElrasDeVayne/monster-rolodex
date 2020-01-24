3daf82d9effc66039a427aa0d6f67ca85f027d9b
Kreshnik Sadriu <sadrikre@students.zhaw.ch>
2020-01-24 17:54:50 +0100

    Updates

---
+++ precache-manifest.dc4f6fc03243d5e2797203911bb3cd8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75dec313108018a9e347f7104f631edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a244c4fe791723193a27",
+    "url": "/static/css/main.3ebe3af4.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a244c4fe791723193a27",
+    "url": "/static/js/main.c3f8eeb9.chunk.js"
+  },
+  {
+    "revision": "863018210bf1c6ae9c68",
+    "url": "/static/js/runtime-main.215bd54d.js"
+  }
+]);
